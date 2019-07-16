e2e9d397c4465f5e4467d45f8ea0f2c146e4050a
Zejing <cameroncao@me.com>
2019-07-16 12:39:53 -0700

    add cart, product etc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Details from "./components/Details";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
 
-function App() {
-  return (
-    <React.Fragment>
-      <h3>Hello</h3>
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
