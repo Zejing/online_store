d4ff75ab37620f3d85039540cad161fb95e3310f
Zejing <cameroncao@me.com>
2019-07-16 14:01:52 -0700

    add cart, detail, not find pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import Default from "./components/Default";
-import Details from "./components/Details";
-import Product from "./components/Product";
 import ProductList from "./components/ProductList";
-
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
